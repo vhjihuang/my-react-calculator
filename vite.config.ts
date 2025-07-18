281fbbbf8083bdef8a20b4f76615b8e51a75b1ec
vhjihuang <1076891184@qq.com>
2025-07-18 13:46:01 +0800

    配置GitHub Pages部署

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/my-react-calculator/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  },
 })
